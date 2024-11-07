a3717f65fab9e57a8aeb6ae5f283df7652788587
Guilherme Nogueira <guiiigen@gmail.com>
2024-11-07 16:47:41 -0300

    Using Async Storage
    Saving and recovering links from storage

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,24 +1,45 @@
-import { useState } from "react";
-import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native";
+import { useState, useCallback } from "react";
+import { View, Image, TouchableOpacity, FlatList, Modal, Text, Alert } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-import { router } from "expo-router";
+import { router, useFocusEffect } from "expo-router";
 
-import { colors } from "@/styles/colors";
 import { Link } from "@/components/link";
 import { Categories } from "@/components/categories";
 import { Option } from "@/components/option";
 
-import { styles } from "./styles";
+import { linkStorage, LinkStorage } from "@/storage/link-storage";
+
 import { categories } from "@/utils/categories";
+
+import { colors } from "@/styles/colors";
+import { styles } from "./styles";
 
 export default function Index(){
     const [category, setCategory] = useState(categories[0].name);
+    const [links, setLinks] = useState<LinkStorage[]>([]);
+
+    async function getLinks() {
+        try {
+            const response = await linkStorage.get();
+            setLinks(response);
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível listar os links")
+        }
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            getLinks();
+            console.log('reload')
+        }, [])
+    )
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-                <TouchableOpacity onPress={()=> router.navigate("/add")}>
+                <TouchableOpacity onPress={() => router.navigate("/add")}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
@@ -26,10 +47,10 @@ export default function Index(){
             <Categories onChange={setCategory} selected={category} />
 
             <FlatList
-                data={["1", "2", "3", "4", "5"]}
-                keyExtractor={item => item}
-                renderItem={() => (
-                    <Link name="Google" url="https://google.com" onDetails={() => console.log("Clicou!")} />
+                data={links}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <Link name={item.name} url={item.url} onDetails={() => console.log("Clicou!")} />
                 )}
                 style={styles.links}
                 contentContainerStyle={styles.linksContent}
